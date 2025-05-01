19ecf580190fbeb251b2d721740f5bdac255a69c
mahditalaldev <mahditalal.789@gmail.com>
2025-05-01 03:18:08 +0300

    frontend/Implement EmailTracker layout with header, search, filter, and email list components

--- frontend/src/pages/EmailTracker/index.jsx
+++ frontend/src/pages/EmailTracker/index.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React from "react";
+import "./EmailTracker.css";
+
+import EmailHeader from "./components/EmailHeader";
+import SearchBox from "./components/SearchBox";
+import FilterGroup from "./components/FilterGroup";
+import EmailList from "./components/EmailList";
+
+import { useEmailFiltering } from "./hooks/useEmailFiltering";
+import { mockEmails } from "./data/mockData";
 
 const EmailTracker = () => {
-  return <div>Email Tracker</div>;
+  const {
+    filteredEmails,
+    filters,
+    searchTerm,
+    setSearchTerm,
+    toggleReadStatus,
+    toggleStarred,
+    handleFilterChange,
+  } = useEmailFiltering(mockEmails);
+
+  return (
+    <div className="page-container">
+      <EmailHeader />
+
+      <div className="gmail-filters">
+        <SearchBox
+          searchTerm={searchTerm}
+          onSearchChange={setSearchTerm}
+          onClearSearch={() => setSearchTerm("")}
+        />
+        <FilterGroup filters={filters} onFilterChange={handleFilterChange} />
+      </div>
+
+      <EmailList
+        emails={filteredEmails}
+        onToggleRead={toggleReadStatus}
+        onToggleStarred={toggleStarred}
+      />
+    </div>
+  );
 };
 
 export default EmailTracker;
