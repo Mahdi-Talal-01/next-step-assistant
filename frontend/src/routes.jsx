e29772f0f235bd56f17c22ece263becdd833e09c
mahditalaldev <mahditalal.789@gmail.com>
2025-05-03 21:03:48 +0300

    frontend/Add Auth route to the application, enabling user authentication with a loading fallback and error handling for improved user experience.

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -12,12 +12,22 @@ const SkillsTrends = React.lazy(() => import("./pages/SkillsTrends"));
 const Roadmaps = React.lazy(() => import("./pages/Roadmap"));
 const AIChat = React.lazy(() => import("./pages/AiChat"));
 const Profile = React.lazy(() => import("./pages/Profile"));
+const Auth = React.lazy(() => import("./pages/Auth"));
 // const Settings = React.lazy(() => import("./pages/Settings"));
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
+    errorElement: <ErrorBoundary />,
+  },
+  {
+    path: "/auth",
+    element: (
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Auth />
+      </React.Suspense>
+    ),
     errorElement: <ErrorBoundary />,
   },
   {
