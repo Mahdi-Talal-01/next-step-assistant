c4352da3448d09d0faf8af4a8409b78d3596c2ee
mahditalaldev <mahditalal.789@gmail.com>
2025-05-08 13:55:41 +0300

    frontend/Add ProtectedRoute component to manage access control, redirecting unauthenticated users to the authentication page while preserving the intended URL, enhancing security and user experience.

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../pages/Auth/hooks/useAuth';
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    // Redirect to auth page but save the attempted url
+    return <Navigate to="/auth" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute; 
