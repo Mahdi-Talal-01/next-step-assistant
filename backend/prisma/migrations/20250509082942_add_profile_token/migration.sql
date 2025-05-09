f5857d2eb92d0c7891e61c0d5f806fcd2251539b
mahditalaldev <mahditalal.789@gmail.com>
2025-05-10 02:04:55 +0300

    backend/prisma/migrations: add Token and Job models with foreign key constraints for user associations, enhancing data structure for OAuth and job application management.

---
+++ backend/prisma/migrations/20250509082942_add_profile_token/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `Token` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `accessToken` TEXT NOT NULL,
+    `refreshToken` TEXT NULL,
+    `expiryDate` BIGINT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `scope` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Token_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Token` ADD CONSTRAINT `Token_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
