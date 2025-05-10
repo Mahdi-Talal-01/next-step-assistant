b8206b66d765a84269186d76a087b86d2aec8c49
mahditalaldev <mahditalal.789@gmail.com>
2025-05-10 10:53:36 +0300

    backend/src/services/GmailService.js: enhance GmailService by adding constructor and defining OAuth scopes for Gmail API integration, laying groundwork for future email functionalities.

--- backend/src/services/GmailService.js
+++ backend/src/services/GmailService.js
@@ -1,5 +1,14 @@
 const { google } = require("googleapis");
 const { TokenRepository } = require("../repositories/TokenRepository");
 
-class GmailService {}
+class GmailService {
+  constructor() {
+    this.scopes = [
+      "https://www.googleapis.com/auth/gmail.readonly",
+      // 'https://www.googleapis.com/auth/gmail.metadata',
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ];
+  }
+}
 module.exports = new GmailService();
