1728ff30667523bfa179c4c57870e395451c9aa4
mahditalaldev <mahditalal.789@gmail.com>
2025-05-13 16:01:12 +0300

    features/Add SkillController to manage skill-related operations, integrating service and repository layers for enhanced skill management functionality.

---
+++ backend/src/controllers/skillController.js
@@ -0,0 +1,7 @@
+const skillService = require('../services/skillService');
+const ResponseTrait = require('../traits/ResponseTrait');
+const skillRepository = require('../repositories/skillRepository');
+class SkillController {}
+
+
+module.exports = new SkillController();
