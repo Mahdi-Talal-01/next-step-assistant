9406b1c825b09d746219ebaa62b38f7ff3f25111
mahditalaldev <mahditalal.789@gmail.com>
2025-05-14 00:16:41 +0300

    feat:contentAssistant/Add content assistant routes to handle requests in contentAssistantRoutes.js

---
+++ backend/src/routes/contentAssistantRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const ContentAssistantController = require('../controllers/ContentAssistantController');
+const ContentAssistantValidation = require('../requests/ContentAssistantValidation');
+
+const router = express.Router();
+
+module.exports = router; 
