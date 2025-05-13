3b24959d805a97e234882c84cdc4cffe25c04d8d
mahditalaldev <mahditalal.789@gmail.com>
2025-05-14 00:24:47 +0300

    feat: contentAssistant/Add generateSchema method to ContentAssistantValidation for validating content generation requests

--- backend/src/requests/ContentAssistantValidation.js
+++ backend/src/requests/ContentAssistantValidation.js
@@ -1,3 +1,21 @@
 const Joi = require('joi');
-class ContentAssistantValidation {}
+class ContentAssistantValidation {
+   /**
+   * Schema for content generation request
+   */
+   static generateSchema = Joi.object({
+    contentType: Joi.string()
+      .valid('jobDescription', 'emailReply', 'linkedinPost', 'blogPost')
+      .required()
+      .messages({
+        'any.required': 'Content type is required',
+        'string.empty': 'Content type cannot be empty',
+        'any.only': 'Content type must be one of: jobDescription, emailReply, linkedinPost, blogPost'
+      }),
+    
+    formData: Joi.object().required().messages({
+      'any.required': 'Form data is required'
+    })
+  });
+}
 module.exports = ContentAssistantValidation;
