fce9e759cefa50e3240acb06b7b0987001a8339a
mahditalaldev <mahditalal.789@gmail.com>
2025-05-15 23:48:49 +0300

    chore: testings/add Jest configuration for testing environment
    - Created jest.config.js to set up testing environment with coverage collection.
    - Configured test matching patterns and specified coverage exclusions.

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.js", "!src/server.js", "!**/node_modules/**"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
