146d2444b51a560f887eea9ff49d48a2c6269c42
mahditalaldev <mahditalal.789@gmail.com>
2025-05-18 00:38:21 +0300

    feat(auth): add request validation classes for login, registration, and OAuth
    Introduce LoginRequest, RegisterRequest, and OAuthRequest classes to handle user authentication requests. Each class includes schema definitions and validation methods using Joi for structured input validation. This enhances the authentication process by ensuring data integrity and providing clear error messages for required fields.

---
+++ backend/src/requests/AuthRequest/RegisterRequest.js
@@ -0,0 +1,42 @@
+const BaseAuthRequest = require('./BaseAuthRequest');
+
+class RegisterRequest extends BaseAuthRequest {
+  static get schema() {
+    return BaseAuthRequest.joi.object({
+      name: BaseAuthRequest.joi.string()
+        .min(2)
+        .required()
+        .messages({
+          'string.min': 'Name must be at least 2 characters long',
+          'string.empty': 'Name is required',
+          'any.required': 'Name is required'
+        }),
+        
+      email: BaseAuthRequest.joi.string()
+        .email()
+        .required()
+        .messages({
+          'string.email': 'Invalid email format',
+          'string.empty': 'Email is required',
+          'any.required': 'Email is required'
+        }),
+        
+      password: BaseAuthRequest.joi.string()
+        .min(6)
+        // .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+        .required()
+        .messages({
+          // 'string.min': 'Password must be at least 6 characters long',
+          // 'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+          'string.empty': 'Password is required',
+          'any.required': 'Password is required'
+        })
+    });
+  }
+
+  static validate(req) {
+    return super.validate(req, this.schema);
+  }
+}
+
+module.exports = RegisterRequest; 
